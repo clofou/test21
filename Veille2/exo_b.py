b0d310e996c2bdafe542d0396ee6ca78f1c1e15f
clofou <fakoro88@gmail.com>
2022-10-11 16:46:22 +0000

    modif

--- Veille2/exo_b.py
+++ Veille2/exo_b.py
@@ -2,7 +2,7 @@
 
 user_input = int(input("Enter a number: "))
 
-number_list = [i for i in range(10)]
+number_list = [i for i in range(11)]
 
 for i in number_list:
     print("{}x{} = {}".format(user_input, i, user_input*i))
